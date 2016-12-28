4f02b37a62f96b44836a64d77024a86a154c67cb
alexiasa <alexiasa@users.noreply.github.com>
2016-12-27 16:53:40 -0800

    working on package fun

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='balanced-pw',
+    version='0.0.2',
+    packages=[''],
+    url='https://github.com/alexiasa/balanced-password/',
+    license='',
+    author='alexiasa',
+    author_email='alexiasa@users.noreply.github.com',
+    description='A Python program to generate balanced diceware passwords',
+    install_requires=['diceware'],
+)
